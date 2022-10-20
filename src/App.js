81aa27320c749c7e53ff3814c1a1cd820ac76d77
Kaelin Salazar <kaelinpsalazar@gmail.com>
2022-10-20 11:30:06 -0600

    trying to deploy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Contact from "./Pages/Contact/Contact";
+import Biography from "./Pages/Biography/Biography"
+import Projects from "./Pages/Projects/Projects"
+import Resume from "./Pages/Resume/Resume"
+import { useState } from "react";
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("Biography");
+  function render() {
+    if (currentPage === "Biography") {
+      return <Biography />;
+    } else if (currentPage === "Contact") {
+      return <Contact />;
+    } else if (currentPage === "Projects") {
+      return <Projects />;
+    } else if (currentPage === "Resume") {
+      return <Resume />;
+    }
+  }
   return (
-    <div className="App">
-      <h1>MyReact App</h1>
-    </div>
+    <>
+      <Navbar setCurrentPage={setCurrentPage} />
+      <div class="renderedData ">{render()}</div>
+      <Footer />
+    </>
   );
 }
 
